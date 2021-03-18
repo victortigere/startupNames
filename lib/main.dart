b78b969999c01bf450a125948dbd3d2b6a5cd518
victortigere <VTigere@firstmutual.co.zw>
2021-03-18 11:24:26 +0200

    splash screen , fullscreen fix

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.purple[200],
+      ),
       home: Scaffold(
       body: Center(
         // child : Text(wordPair.asPascalCase),
@@ -26,25 +29,74 @@ class RandomWords extends StatefulWidget{
 class _RandomWordsState extends State<RandomWords> {
 
   final _suggestions = <WordPair>[];
+  final _saved = <WordPair>{};
   final _biggerFont = TextStyle(fontSize: 18.0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('startUp Name Generator'),
+        title: Text('Zens'),
+        actions: [
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
-
   }
 
+  void _pushSaved(){
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            final tiles = _saved.map(
+                (WordPair pair) {
+                  return ListTile(
+                    title: Text(
+                      pair.asPascalCase,
+                      style: _biggerFont,
+                    ),
+                  );
+                },
+            );
+
+            final divided = ListTile.divideTiles(
+              context: context,
+              tiles:tiles,
+            ).toList();
+
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('Saved Suggestions'),
+              ),
+              body: ListView(children: divided),
+            );
+          },
+      ),
+    );
+
+  }//_pushSaved
+
     Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
       return ListTile(
         title: Text(
           pair.asPascalCase,
           style: _biggerFont,
         ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.red : null,
+        ),
+        onTap: (){
+          setState(() {
+            if(alreadySaved){
+              _saved.remove(pair);
+            }else{
+              _saved.add(pair);
+            }
+          });
+        },
       );
     }
 
